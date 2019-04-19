00c99a85232f29a9bf4fd7b9f9c914c393d57097
HenryGuo <guohongyi@sjtu.edu.cn>
2019-04-19 23:33:41 +0800

    fix butgs

--- Project/fully_connected.py
+++ Project/fully_connected.py
@@ -9,27 +9,16 @@ MAX_EPOCHES = int(1e6)
 
 
 class FullyConnected(object):
-    def __init__(self, name, logger, lr, lr_decay, n_classes, max_epoches, train_data, train_label,
-                 test_data=None, test_label=None, seed=0):
+    def __init__(self, name, lr, lr_decay, n_classes, max_epoches, train_data, train_label,
+                 test_data=None, test_label=None, seed=0, to_test=False):
         np.random.seed(seed)
         tf.set_random_seed(seed)
 
-        logger.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s\t%(message)s')
+        assert to_test is False or test_data is not None
 
-        stdout_handler = logging.StreamHandler(sys.stdout)
-        stdout_handler.setLevel(logging.INFO)
-        stdout_handler.setFormatter(formatter)
-        logger.addHandler(stdout_handler)
-
-        file_handler = logging.FileHandler('logs/%s.log' % name)
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-
-        self.logger = logger
         self.name = name
-        self.to_test = (test_data is not None)
+        self.logger = self.getLogger()
+        self.to_test = to_test
         self.train_data = np.array(train_data)
         self.train_label = np.squeeze(train_label)
         self.test_data = np.array(test_data)
@@ -39,28 +28,47 @@ class FullyConnected(object):
         self.lr = lr
         self.lr_decay = lr_decay
         self.max_epoches = max_epoches
-        self.sess = tf.Session()
 
-        self.LR = tf.placeholder(tf.float32, [], 'learning_rate')
-        self.X = tf.placeholder(tf.float32, [None, self.feature_dim], 'obs')
-        self.Y = tf.placeholder(tf.int32, [None], 'label')
+        self.graph = tf.Graph()
+        self.sess = tf.Session(graph=self.graph)
 
-        with tf.variable_scope('model_%s' % self.name):
+        with self.graph.as_default():
+            self.LR = tf.placeholder(tf.float32, [], 'learning_rate')
+            self.X = tf.placeholder(tf.float32, [None, self.feature_dim], 'obs')
+            self.Y = tf.placeholder(tf.int32, [None], 'label')
+
             self.logits_ = tf.layers.dense(
                 self.X,
                 self.n_classes,
                 kernel_initializer=tf.random_normal_initializer
             )
+            self.loss_ = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+                logits=self.logits_,
+                labels=self.Y
+            ))
+            self.opt_ = tf.train.GradientDescentOptimizer(self.LR).minimize(self.loss_)
 
-        self.loss_ = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-            logits=self.logits_,
-            labels=self.Y
-        ))
+            self.sess.run(tf.global_variables_initializer())
 
-        self.opt_ = tf.train.GradientDescentOptimizer(self.LR).minimize(self.loss_)
-        self.sess.run(tf.global_variables_initializer())
+    def getLogger(self):
+        logger = logging.getLogger(self.name)
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s\t%(message)s\tmodel:' + self.name)
+
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(logging.INFO)
+        stdout_handler.setFormatter(formatter)
+        logger.addHandler(stdout_handler)
+
+        file_handler = logging.FileHandler('logs/%s.log' % self.name)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        return logger
 
     def train(self):
+        print('1')
         for ep in range(self.max_epoches):
             lr = self.lr * (1 - ep/self.max_epoches) if self.lr_decay else self.lr
             loss, _ = self.sess.run([self.loss_, self.opt_], feed_dict={
@@ -73,13 +81,16 @@ class FullyConnected(object):
                     self.logger.info('ep:%i\t loss:%f\t acc:%f' % (ep, loss, self.test()))
                 else:
                     self.logger.info('ep:%i\tloss:%f' % (ep, loss))
+                    print('ep:%i\tloss:%f' % (ep, loss))
         print('model %s finished training' % self.name)
 
     def test(self):
         logits = self.sess.run(self.logits_, feed_dict={self.X: self.test_data})
         labels = np.argmax(logits, axis=-1)
-        acc = np.count_nonzero(labels==self.test_label) / self.test_label.size
-        return acc
+        if self.test_label is not None:
+            return np.count_nonzero(labels==self.test_label) / self.test_label.size
+        else:
+            return labels
 
     def classify(self, X):
         logits = self.sess.run(self.logits_, feed_dict={self.X: X})
@@ -97,7 +108,6 @@ def main():
     
     model = FullyConnected(
         name='fully_connected',
-        logger=logging.getLogger('fully_connected'),
         lr=LEARNING_RATE,
         lr_decay=False,
         n_classes=4,
@@ -105,7 +115,8 @@ def main():
         train_data=train_de,
         train_label=train_label_eeg,
         test_data=test_de,
-        test_label=test_label_eeg
+        test_label=test_label_eeg,
+        to_test=True
     )
     model.train()
 
--- Project/random_decomposition.py
+++ Project/random_decomposition.py
@@ -1,17 +1,17 @@
-import scipy.io as sio
+import os
 import random
 import argparse
 import numpy as np
-import tensorflow as tf
+import scipy.io as sio
 from fully_connected import FullyConnected
 from utils import empty_list
-from multiprocessing import Process
-import logging
+from multiprocessing import Pool, Process
 
 
 def parse_arg():
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', type=int, default=3)
+    parser.add_argument('-seed', type=int, default=0)
     parser.add_argument('-lr', type=float, default=5e-5)
     parser.add_argument('-lr_decay', type=bool, default=False)
     parser.add_argument('-serial', action='store_true', default=False)
@@ -21,112 +21,88 @@ def parse_arg():
     parser.add_argument('-train_label', type=str, default='train_label_eeg')
     parser.add_argument('-test_data', type=str, default='test_de')
     parser.add_argument('-test_label', type=str, default='test_label_eeg')
+    parser.add_argument('-n_processes', type=int, default=4)
+
     return parser.parse_args()
 
 
 """
 divide data from this class and from other class into $n$ parts respectively
 """
-def decomposition(args, data1, label1, data0, label0, c):
-    _data1 = empty_list(args.n)
-    _label1 = empty_list(args.n)
-    _data0 = empty_list(args.n)
-    _label0 = empty_list(args.n)
-
-    for d, l in zip(data1, label1):
-        r = random.randint(0, args.n-1)
-        _data1[r].append(d)
-        _label1[r].append(l)
-
-    for d, l in zip(data0, label0):
-        r = random.randint(0, args.n - 1)
-        _data1[r].append(d)
-        _label1[r].append(l)
-
-    models = []
-    for i, (d1, l1) in enumerate(zip(_data1, _label1)):
-        for j, (d0, l0) in enumerate(zip(_data0, _label0)):
-            name = '%i_%i_%i' % (c, i, j)
-            models.append(FullyConnected(
-                name=name,
-                logger=logging.getLogger(name),
-                lr=args.lr,
-                lr_decay=args.lr_decay,
-                n_classes=2,
-                max_epoches=args.max_epoches,
-                train_data=(d1 + d0),
-                train_label=(l1 + l0)))
-    return models
-
-"""
-train only one model
-"""
-def train_one_model(model):
-    model.train()
-
-
-"""
-train our models serially
-"""
-def train(args, models):
-    for c in range(args.n_classes):
-        for m in models[c]:
-            m.train()
-
-
-"""
-train our models in parallel
-"""
-def train_in_parallel(args, models):
-    processes = []
-    for c in range(args.n_classes):
-        for m in models[c]:
-            processes.append(Process(target=train_one_model, args=(m,)))
-            processes[-1].start()
-    for p in processes:
-        p.join()
-
-
-def main():
-    args = parse_arg()
-
+def decomposition(c):
     data = sio.loadmat('data.mat')
     train_d = data[args.train_data]
     train_l = data[args.train_label]
     test_d = data[args.test_data]
     test_l = data[args.test_label]
 
-    train_data = empty_list(args.n_classes, 2)
-    train_label = empty_list(args.n_classes, 2)
+    data0 = []
+    label0 = []
+    data1 = []
+    label1 = []
 
-    models = []
-    for c in range(args.n_classes):
-        for d, l in zip(train_d, train_l):
-            if l == c:
-                train_data[c][0].append(d)
-                train_label[c][0].append(1)
-            else:
-                train_data[c][1].append(d)
-                train_label[c][1].append(0)
-        models.append(decomposition(args, train_data[c][0], train_label[c][0], train_data[c][1], train_label[c][1], c))
+    _data1 = empty_list(args.n)
+    _label1 = empty_list(args.n)
+    _data0 = empty_list(args.n)
+    _label0 = empty_list(args.n)
 
-    # training
+    for d, l in zip(train_d, train_l):
+        if l == c:
+            data1.append(d)
+            label1.append(1)
+        else:
+            data0.append(d)
+            label0.append(0)
+
+    for d, l in zip(data1, label1):
+        r = random.randint(0, args.n - 1)
+        _data1[r].append(d)
+        _label1[r].append(l)
+
+    for d, l in zip(data0, label0):
+        r = random.randint(0, args.n - 1)
+        _data0[r].append(d)
+        _label0[r].append(l)
+
+    max_labels = []
+    for i, (d1, l1) in enumerate(zip(_data1, _label1)):
+        min_labels = []
+        for j, (d0, l0) in enumerate(zip(_data0, _label0)):
+            model = FullyConnected(
+                name='%i_%i_%i' % (c, i, j),
+                lr=args.lr,
+                lr_decay=args.lr_decay,
+                n_classes=2,
+                max_epoches=args.max_epoches,
+                train_data=d1 + d0,
+                train_label=l1 + l0,
+                test_data=test_d,
+                seed=args.seed
+            )
+            model.train()
+            min_labels.append(model.test())
+        max_labels.append(np.min(min_labels, axis=0))
+
+    return np.max(max_labels, axis=0)
+
+
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+args = parse_arg()
+
+
+def main():
     if args.serial:
-        train(args, models)
-    else:
-        train_in_parallel(args, models)
-
-    predicts = []
-    for c in range(args.n_classes):
         results = []
-        for m in models[c]:
-            results.append(m.classify(test_d))
-        _results = []
-        for i in range(0, len(results), args.n):
-            _results.append(np.min(results[i:i+args.n], axis=0))
-        results = np.max(_results, axis=0)
-        predicts.append(results * (c+1))  # 0->0, 1->(c+1)
+        for c in range(args.n_classes):
+            results.append(decomposition(c))
+    else:
+        pool = Pool(args.n_processes)
+        results = pool.map(decomposition, range(args.n_classes))
 
+    """ collect """
+    test_l = sio.loadmat('data.mat')[args.test_label]
+    predicts = np.argmax(results, axis=0)
     acc = np.count_nonzero(test_l[:, 0] + 1 == np.argmax(predicts, axis=0)) / test_l.shape[0]
     print('acc =', acc)
 

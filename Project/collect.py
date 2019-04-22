32fe95a5e14b707c5ae9c8f7340bb4b04bf66470
HenryGuo <guohongyi@sjtu.edu.cn>
2019-04-22 15:27:12 +0800

    typo

--- Project/collect.py
+++ Project/collect.py
@@ -13,14 +13,13 @@ for root, dirs, files in os.walk('logs'):
                 line = line.split()
                 for x in line:
                     x = x.split(':')
-                    if len(x) == 1:
-                        if x[0] == 'ep' and x[1] == '0':
-                            loss = []
-                            acc = []
-                        if x[0] == 'loss':
-                            loss.append(float(x[1]))
-                        if x[0] == 'acc':
-                            acc.append(float(x[1]))
+                    if x[0] == 'ep' and x[1] == '0':
+                        loss = []
+                        acc = []
+                    if x[0] == 'loss':
+                        loss.append(float(x[1]))
+                    if x[0] == 'acc':
+                        acc.append(float(x[1]))
 
             if len(loss) > 0:
                 plt.plot(loss)
